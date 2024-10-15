a8d3ff0a810cc306c380a0a63daee70d2740ea8d
Ivan Christian <120652762+christvann@users.noreply.github.com>
2024-10-15 14:45:34 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const header = document.querySelector("header");
-window.addEventListener("scroll", function() {
-    header.classList.toggle("sticky", window.scrollY > 150);
-});
-
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
 };
+const sr = ScrollReveal({
+  distance: "65px",
+  duration: 2600,
+  delay: 450,
+  reset: true,
+});
 
-window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navlist.classList.remove('open');
-};
+sr.reveal(".cars-text", { delay: 200, origin: "top" });
+sr.reveal(".cars-img", { delay: 450, origin: "top" });
+sr.reveal(".icons", { delay: 500, origin: "left" });
+sr.reveal(".scroll-down", { delay: 500, origin: "right" });
